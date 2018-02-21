14874664178275fe16d6d8a2bc00f0ba76c9482b
Nicholas Richardson <nicholas.richardson.05@gmail.com>
2018-02-21 22:35:18 +0000

    created first angular web app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
+
+
+
 
 
 import { AppComponent } from './app.component';
 
+import { BooksComponent } from './books/books.component';
+
+
+
+
 
 @NgModule({
+
   declarations: [
-    AppComponent
+
+    AppComponent,
+
+    BooksComponent
+
   ],
+
   imports: [
+
     BrowserModule
+
   ],
+
   providers: [],
+
   bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }
